605fcbe95edd53fbe83d7d30cf7a7a25ffc8e0b1
Jon Whittlestone <dev@howapped.com>
2023-08-23 06:49:27 +0100

    03 Birthday CLI app with weeks

--- src/03-birthday.ts
+++ src/03-birthday.ts
@@ -1,6 +1,7 @@
 
 var readline = require('readline');
 var util = require('util');
+const moment = require('moment'); // Import the moment.js library
 
 function heading(heading: string): void {
     const figlet = require("figlet");
@@ -24,6 +25,19 @@ var rl = readline.createInterface({
 
 var question = util.promisify(rl.question).bind(rl);
 
+function getWeeksUntilBirthday(targetDate: Date): number {
+  const currentDate = new Date();
+  const targetBirthday = new Date(targetDate);
+  targetBirthday.setFullYear(currentDate.getFullYear());
+
+  if (targetBirthday < currentDate) {
+    targetBirthday.setFullYear(currentDate.getFullYear() + 1);
+  }
+
+  const timeDiff = targetBirthday.getTime() - currentDate.getTime();
+  const weeksRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24 * 7));
+  return weeksRemaining;
+}
 
 async function ask() {
 
@@ -33,6 +47,13 @@ async function ask() {
         ["What month were you born [MM]"],
         ["What day were you born [DD]"]
     ];
+
+    type AskInstance = {
+        question: string;
+        answer: number;
+
+    }
+
     for(var i = 0;i<questions.length;i++){
         try {
             questions[i][1] = parseInt(await question(`${questions[i][0]}\n`));
@@ -40,16 +61,19 @@ async function ask() {
             console.error('Question rejected', err);
         }
     }
-    // const y_m_d = questions.map((q) => q[1]);
     console.log("\nThese were your answers:");
     console.table(questions)
-    console.log("\nIt looks like you were born on:");
-    if (typeof questions[0][1] === 'number' && typeof questions[1][1] === 'number' && typeof questions[2][1] === 'number') {
-        console.log(`\n${new Date(
-            questions[0][1],
-            questions[1][1]-1,
-            questions[2][1]
-        )}`)
+    if (typeof questions[0][1] === 'number' && 
+        typeof questions[1][1] === 'number' &&
+        typeof questions[2][1] === 'number'
+    ) {
+        const currentDate = moment();
+        const currentWeek = currentDate.isoWeek();
+        console.log(`\nThe current week number: ${currentWeek}`);
+        const dt = new Date(questions[0][1], questions[1][1] - 1, questions[2][1]);
+        console.log(`\nIt looks like you were born on: ${dt}`);
+       const weeksUntilBirthday = getWeeksUntilBirthday(dt);
+       console.log(`Your birthday is in ${weeksUntilBirthday} weeks `);
     }
     rl.close();
 }
